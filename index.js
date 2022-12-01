5f66c6fde3414922859b887912c66d4496ee41a8
Joseph <Jbonventre95@gmail.com>
2022-12-01 10:05:41 -0500

    commit tree 7101b80272c8addfb2d1c4d4771e90d49c5b57d0

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let newWord = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    newWord += word[i];
+  }
+
+  return newWord === word ? true : false;
 }
 
 /* 
